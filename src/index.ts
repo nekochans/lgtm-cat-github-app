4cffbfd018ed8a42ba2b9d6c8b61db8f0d425a4b
kobayashi-m42 <m42kobayashi@gmail.com>
2025-12-15 07:46:54 +0900

    Probotを使用したGitHub Appのローカル開発環境を構築

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { Probot } from "probot";
+
+export default (app: Probot) => {
+  app.log.info("LGTM Cat GitHub App is running!");
+
+  app.on("pull_request_review.submitted", async (context) => {
+    const { review, pull_request, repository } = context.payload;
+
+    app.log.info({
+      action: "pull_request_review.submitted",
+      repo: repository.full_name,
+      pr: pull_request.number,
+      reviewer: review.user?.login ?? "unknown",
+      state: review.state,
+    });
+
+    if (review.state === "approved") {
+      app.log.info("PR was approved! Ready to post LGTM cat image.");
+      // TODO: LGTM cat image posting will be implemented in the next issue
+    }
+  });
+};
